2cc413e1921aaac92854890c1893e56720a45b2b
yang <2689991790@qq.com>
2021-08-22 01:16:05 +0800

    init

---
+++ mini-webpack/src/add.js
@@ -0,0 +1,6 @@
+// export default function add(a, b) {
+//   return a + b;
+// }
+exports.default = function (a, b) {
+  return a + b;
+};
---
+++ mini-webpack/src/index.js
@@ -0,0 +1,3 @@
+// import add from "./add";
+var add = require('./add.js').default
+console.log(add(1,2))
