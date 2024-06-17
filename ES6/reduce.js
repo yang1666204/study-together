b002ee16dedccb5fe8cd69ab683e07eff69edbf7
yang <2689991790@qq.com>
2024-06-17 22:08:37 +0800

    调整目录结构

--- ES6/reduce.js
+++
@@ -1,8 +0,0 @@
-let color = ['red','black','yellow']
-
-let newArr = color.reduce((pre,cur)=>{
-    console.log("pre",pre);
-    console.log("cur",cur);
-    return pre+cur
-})
-console.log(newArr);
