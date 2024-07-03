b8fef1b976a19f149b70749af2aa736ba37a2e08
yang <2689991790@qq.com>
2024-07-03 23:45:07 +0800

    leetcode:26 删除有序数组中的重复项（双指针实现）

---
+++ 数据结构&算法/数组/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,33 @@
+/**
+ * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
+ * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
+ * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
+ * 返回 k 。
+ * 
+ * 示例 2：
+ *
+ * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
+ * 输出：5, nums = [0,1,2,3,4]
+ * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function (nums) {
+    let slow = 0, fast = 0;
+    while (fast < nums.length) {
+        if (nums[slow - 1] < nums[fast]) {
+            nums[slow] = nums[fast];
+            fast++;
+            slow++
+        } else {
+            fast++;
+            if (slow === 0 || nums[slow] > nums[slow - 1]) {
+                slow++
+            }
+        }
+    }
+    return slow;
+};
