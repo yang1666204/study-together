b114a620015cabad1fa76505abf4443dcfa64795
yang <2689991790@qq.com>
2024-07-06 10:42:18 +0800

    删除数组重复项（双指针）

--- 数据结构&算法/数组/remove-duplicates-from-sorted-array.js
+++ 数据结构&算法/数组/remove-duplicates-from-sorted-array.js
@@ -1,34 +1,34 @@
 /**
- * 26. 删除有序数组中的重复项
- * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现一次 ，返回删除后数组的新长度。元素的 相对顺序 应该保持 一致 。然后返回 nums 中唯一元素的个数。
- * 考虑 nums 的唯一元素的数量为 k ，你需要做以下事情确保你的题解可以被通过：
- * 更改数组 nums ，使 nums 的前 k 个元素包含唯一元素，并按照它们最初在 nums 中出现的顺序排列。nums 的其余元素与 nums 的大小不重要。
- * 返回 k 。
+ * 题目描述：
+ * 给你一个 非严格递增排列 的数组 nums ，请你 原地 删除重复出现的元素，使每个元素 只出现 K 次 ，返回删除后数组的新长度。
+ * 元素的 相对顺序 应该保持 一致 。
+ * 
+ * 相关题目：26. 删除有序数组中的重复项 80. 删除有序数组中的重复项 II
  * 
  * 示例 2：
  *
- * 输入：nums = [0,0,1,1,1,2,2,3,3,4]
- * 输出：5, nums = [0,1,2,3,4]
- * 解释：函数应该返回新的长度 5 ， 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4 。不需要考虑数组中超出新长度后面的元素。
+ * 输入：nums = [0,0,1,1,1,2,2,2,3] k = 2
+ * 输出：5, nums = [0,0,1,1,2,2,3]
  */
 
 /**
- * @param {number[]} nums
- * @return {number}
+ * 
+ * @param {number[]} nums 
+ * @param {number} k 
  */
-var removeDuplicates = function (nums) {
+var removeDuplicates = function (nums, k) {
+    /**
+     * slow:当前可以插入的位置指针
+     * fast:遍历slow之后的每一个元素的指针
+     * 如果slow - k的位置不等于fast指向的元素，说明slow这个位置是可以插入的
+     */
     let slow = 0, fast = 0;
     while (fast < nums.length) {
-        if (nums[slow - 1] < nums[fast]) {
+        if (nums[slow - k] !== nums[fast]) {
             nums[slow] = nums[fast];
-            fast++;
-            slow++
-        } else {
-            fast++;
-            if (slow === 0 || nums[slow] > nums[slow - 1]) {
-                slow++
-            }
+            slow++;
         }
+        fast++;
     }
     return slow;
 };
