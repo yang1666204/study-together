4d5231c72ac5d7ef4e15b9dae2369ee7df38a4a3
yang <2689991790@qq.com>
2024-07-27 22:57:45 +0800

    prettier code

--- i18n-tool/src/index.js
+++ i18n-tool/src/index.js
@@ -4,10 +4,10 @@ const parser = require('@babel/parser');
 const traverse = require('@babel/traverse').default;
 const types = require('@babel/types');
 const template = require('@babel/template').default;
+const format = require('prettier-eslint')
 const { codeFrameColumns } = require('@babel/code-frame');
 const generator = require('@babel/generator').default;
 const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
-
 
 async function traversalFiles(absPath, filePaths) {
     const filelist = await fse.readdir(absPath);
@@ -41,15 +41,15 @@ async function getFileList(absPath) {
  * @param {parser.ParseResult<types.File>} ast 
  * @param {{fileContent: string;filePath: string;extname: string}} file 
  */
-function handleAst(ast, file) {
+async function handleAst(ast, file) {
     traverse(ast, {
         // 注入 import { intl } from '@/utils/intl';
         Program: {
             enter: (path, state) => {
                 let imported = false;
                 path.traverse({
-                    ImportDeclaration(p) {
-                        if (p.node.source.value === 'intl') {
+                    ImportSpecifier(p) {
+                        if (p.node.local.name === 'intl') {
                             imported = true;
                         }
                     }
@@ -72,11 +72,35 @@ function handleAst(ast, file) {
         }
     })
     const { code } = generator(ast);
-    saveCode(code, file.filePath)
+    const formatedCode = await prettierCode(code)
+    saveCode(formatedCode, file.filePath)
 }
 
 function saveCode(code, filePath) {
     fse.writeFile(filePath, code);
+}
+
+async function prettierCode(code) {
+    const options = {
+        text: code,
+        eslintConfig: {
+            parserOptions: {
+                ecmaVersion: 7
+            },
+            rules: {
+                semi: ['error', 'never']
+            }
+        },
+        prettierOptions: {
+            bracketSpacing: true
+        },
+        fallbackPrettierOptions: {
+            singleQuote: false
+        }
+    };
+
+    const formatted = await format(options);
+    return formatted
 }
 
 async function pipeline(entry) {
@@ -89,7 +113,7 @@ async function pipeline(entry) {
             sourceType: 'unambiguous',
             plugins: ['jsx']
         });
-        handleAst(ast, file)
+        await handleAst(ast, file)
     }
 }
 const entry = './example/src';
